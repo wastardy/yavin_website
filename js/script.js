ac689caea0ab59dfaecc0abc658b61d701cbc221
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-09 19:45:16 +0200

    add stats section and js code to increment numbers in stats

--- js/script.js
+++ js/script.js
@@ -13,6 +13,35 @@ const userScroll = () => {
 
 document.addEventListener('DOMContentLoaded', userScroll);
 
+const incrementStats = () => {
+    const counters = document.querySelectorAll('.counter');
+
+    counters.forEach((counter) => {
+        counter.innerText = 0;
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const number = +counter.innerText;
+
+            const increment = target / 200; // increment speed
+
+            if (number < target) {
+                counter.innerText = Math.ceil(number + increment);
+                
+                // call it recusively
+                setTimeout(updateCounter, 1);
+            }
+            else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', incrementStats);
+
 document.addEventListener('click', (event) => {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('#nav-dropdown');
