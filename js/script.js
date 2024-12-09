dbd5d912e5fcb95a5c82d4e331bd4ac802bf37ee
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-09 16:36:44 +0200

    add script for navbar at scroll and edit navbar paddings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const userScroll = () => {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 55) {
+            navbar.classList.add('navbar-background');
+        }
+        else {
+            navbar.classList.remove('navbar-background')
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+
 document.addEventListener('click', (event) => {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('#nav-dropdown');
